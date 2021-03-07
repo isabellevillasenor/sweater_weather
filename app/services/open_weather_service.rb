3d854fbb8ded7c29a53380603f8744c3c5d49e61
Isabelle Villasenor <villasenori@outlook.com>
2021-03-07 15:47:25 -0700

    Add Units Imperial to OpenWeatherService

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,6 +1,6 @@
 class OpenWeatherService
   def self.get_weather(lat, lng)
-    conn = Faraday.new("http://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lng}&appid=#{ENV['OPEN_WEATHER_API_KEY']}")
+    conn = Faraday.new("http://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lng}&appid=#{ENV['OPEN_WEATHER_API_KEY']}&units=imperial")
     response = conn.get
     JSON.parse(response.body, symbolize_names: true)
   end
