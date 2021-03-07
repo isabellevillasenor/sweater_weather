fbfc8bfee3f5634a87a453f8f8cc77c2aab4d25a
Isabelle Villasenor <villasenori@outlook.com>
2021-03-07 16:52:57 -0700

    Refactor Data Passing to HourlyWeather PORO and PORO Spec

--- app/poros/hourly_weather.rb
+++ app/poros/hourly_weather.rb
@@ -4,9 +4,9 @@ class HourlyWeather
               :conditions,
               :icon
   def initialize(data)
-    @time = Time.zone.at(data[:hourly][0][:dt]).strftime('%H:%M:%S')
-    @temp = data[:hourly][0][:temp].round(1)
-    @conditions = data[:hourly][0][:weather][0][:description]
-    @icon = data[:hourly][0][:weather][0][:icon]
+    @time = Time.zone.at(data[:dt]).strftime('%H:%M:%S')
+    @temp = data[:temp].round(1)
+    @conditions = data[:weather][0][:description]
+    @icon = data[:weather][0][:icon]
   end
 end
--- spec/poros/hourly_weather_poro_spec.rb
+++ spec/poros/hourly_weather_poro_spec.rb
@@ -5,7 +5,7 @@ describe HourlyWeather do
     json_response = File.read('spec/fixtures/openweather_forecast.json')
     result = JSON.parse(json_response, symbolize_names: true)
 
-    hourly_weather = HourlyWeather.new(result)
+    hourly_weather = HourlyWeather.new(result[:hourly][0])
 
     expect(hourly_weather.time).to eq('22:00:00')
     expect(hourly_weather.temp).to eq(68.4)
