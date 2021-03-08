6b34ca266d724ff0f797a063447b31b01c6eb89b
Isabelle Villasenor <villasenori@outlook.com>
2021-03-08 12:02:30 -0700

    Add Travel Time Method to Facade and Facade Spec

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -4,5 +4,20 @@ class MapQuestFacade
       json = MapQuestService.get_coords(location)
       json[:results][0][:locations][0][:latLng]
     end
+
+    def travel_time(start, destination)
+      json = MapQuestService.travel_time(start, destination)
+      @time = Time.at(json[:route][:formattedTime].to_time).to_i
+      
+      if json[:info][:statuscode] = 0
+        trip = Travel.new(json, start, destination)
+        weather = OpenWeatherService.get_weather(json[:route][:locations][1][:displayLatLng][:lat],
+                                                 json[:route][:locations][1][:displayLatLng][:lng])
+        # destination_weather = HourlyWeather.new(weather[:hourly].select{|hash| hash[:dt] == @time})
+        destination_weather = TripForecast.new(weather, @time)
+        trip.add_forecast(destination_weather)
+        trip
+      end
+    end
   end
 end
--- spec/facades/map_quest_facade_spec.rb
+++ spec/facades/map_quest_facade_spec.rb
@@ -23,5 +23,30 @@ describe MapQuestFacade do
       expect(response[:lng]).to be_a Float
       expect(response[:lng]).to eq(-104.984853)
     end
+
+    it '.travel_time' do
+      json_response = File.read('spec/fixtures/mapquest_travel_search_2.json')
+      stub_request(:get, "http://open.mapquestapi.com/directions/v2/route?from=Denver,CO&key=#{ENV['MAPQUEST_API_KEY']}&to=Pueblo,CO").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.3.0'
+           }).
+         to_return(status: 200, body: json_response, headers: {})
+
+      weather_response = File.read('spec/fixtures/openweather_pueblo_forecast.json')
+      stub_request(:get, "http://api.openweathermap.org/data/2.5/onecall?appid=2d39d6d4253ea35c67fdf1d6b563a7d5&lat=38.265427&lon=-104.610413&units=imperial").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.3.0'
+           }).to_return(status: 200, body: weather_response, headers: {})
+
+      response = MapQuestFacade.travel_time('Denver,CO', 'Pueblo,CO')
+
+      expect(response).to be_a Travel
+    end
   end
 end
