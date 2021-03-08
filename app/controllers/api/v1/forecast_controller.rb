b6554913973edea8426cfd157c7f5c0e43272470
Isabelle Villasenor <villasenori@outlook.com>
2021-03-07 17:52:49 -0700

    Add Forecast Request Spec and Forecast Controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def show
     coords = MapQuestFacade.get_coords(params[:location])
-    # forecast = get_weather(coords[:lat], coords[:lng])
+    forecast = OpenWeatherFacade.get_weather(coords[:lat], coords[:lng])
+    render json: ForecastSerializer.new(forecast)
   end
 end
