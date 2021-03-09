36f4c3b75fb766b473f69e0bfe4fdbe98e4907ca
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 07:14:06 -0700

    Refactor ImageSerializer

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,10 +1,5 @@
 class ImageSerializer
-  include JSONAPI::Serializer
-  attribute :image do |image|
-    {
-      location: image.location,
-      image_url: image.image_url,
-      credit: image.credit
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  set_id {nil}
+  attributes :image
 end
