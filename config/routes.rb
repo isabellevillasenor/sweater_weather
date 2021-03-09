189bcd9bfee7e40b39a193b8d7860bbd1467677e
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 13:00:17 -0700

    Add Users Create Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/forecast', to: 'forecast#show'
+      resources :forecast, only: [:show]
       get '/backgrounds', to: 'images#show'
+      resources :users, only: [:create]
     end
   end
 end
