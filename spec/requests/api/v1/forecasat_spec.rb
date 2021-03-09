e8696d16bc87cf52ede34b252532592eea5f168b
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 14:58:29 -0700

    Add Sad Path Testing Forecast Controller

--- spec/requests/api/v1/forecasat_spec.rb
+++ spec/requests/api/v1/forecasat_spec.rb
@@ -26,4 +26,12 @@ describe 'Forecast Controller' do
       expect(json[:data][:attributes][:hourly_weather][0].count).to eq(4)
     end
   end
+
+  it 'sends an error message if unable to find location/weather' do
+    VCR.use_cassette('retrive_city_weather') do
+      get '/api/v1/forecast?location='
+
+      expect(response.status).to eq(404)
+    end
+  end
 end
