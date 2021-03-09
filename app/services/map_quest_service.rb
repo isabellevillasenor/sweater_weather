f71af42ca06b78f4b072a0d70c5117f81b610f0b
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 11:53:29 -0700

    Add Travel Time Method to Service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -5,5 +5,11 @@ class MapQuestService
       response = conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
       JSON.parse(response.body, symbolize_names: true)
     end
+
+    def travel_time(start, destination)
+      conn = Faraday.new("https://open.mapquestapi.com")
+      response = conn.get("/directions/v2/route?from=#{start}&key=#{ENV['MAPQUEST_API_KEY']}&to=#{destination}")
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
