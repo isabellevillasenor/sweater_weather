cc7b8eb278c4988c9549a45883136c90fe364df3
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 11:53:51 -0700

    Add Travel Time Method to Facade

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -4,5 +4,12 @@ class MapQuestFacade
       json = MapQuestService.get_coords(location)
       json[:results][0][:locations][0][:latLng]
     end
+
+    def travel_time(start, destination)
+      json = MapQuestService.travel_time(start, destination)
+      # @time = Time.at(json[:route][:formattedTime].to_time).to_i
+      #iterate throught the 3 responses and turn them into Trail PORO
+
+    end
   end
 end
