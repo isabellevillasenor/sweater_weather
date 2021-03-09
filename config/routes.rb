267e3c00ec9e514288dbb269b87e204286dd2583
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 13:35:19 -0700

    Refactor Forecast Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forecast, only: [:show]
+      get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'images#show'
       resources :users, only: [:create]
     end
