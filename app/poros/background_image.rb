f276f7a482140aa76dfdc46f9055ed54c2f82219
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 07:11:00 -0700

    Refactor Background Image PORO and PORO Spec

--- app/poros/background_image.rb
+++ app/poros/background_image.rb
@@ -1,21 +1,14 @@
 class BackgroundImage
-  attr_reader :id,
-              :location,
-              :image_url,
-              :credit
+  attr_reader :image
   def initialize(data, location)
-    @id = nil
-    @location = location
-    @image_url = data[:urls][:full]
-    @credit = create_credit(data)
-  end
-
-  def create_credit(data)
-    {
-      source: 'https://unsplash.com/',
-      author: data[:user][:name],
-      author_username: data[:user][:username],
-      logo: data[:user][:links][:html]
-    }
+    @image = {
+              location: location,
+              image_url: data[:urls][:full],
+              credit: {
+                source: 'https://unsplash.com/',
+                author: data[:user][:name],
+                logo: data[:user][:links][:html]
+              }
+            }
   end
 end
--- spec/poros/background_image_poro_spec.rb
+++ spec/poros/background_image_poro_spec.rb
@@ -7,18 +7,17 @@ describe BackgroundImage do
 
     background_image = BackgroundImage.new(response[:results][0], 'denver,co')
 
-    expect(background_image.id).to be_nil
-    expect(background_image.location).to eq('denver,co')
-    expect(background_image.image_url).to eq('https://images.unsplash.com/photo-1599408169542-620fc453382c?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTMwNjJ8MHwxfHNlYXJjaHwxfHxkZW52ZXJ8ZW58MHx8fHwxNjE1MjUyMTcw&ixlib=rb-1.2.1&q=85')
+    expect(background_image.image).to be_a Hash
 
-    expect(background_image.credit).to be_a Hash
-    expect(background_image.credit).to have_key(:source)
-    expect(background_image.credit[:source]).to eq('https://unsplash.com/')
-    expect(background_image.credit).to have_key(:author)
-    expect(background_image.credit[:author]).to eq('Andrew Coop')
-    expect(background_image.credit).to have_key(:author_username)
-    expect(background_image.credit[:author_username]).to eq('andrewcoop')
-    expect(background_image.credit).to have_key(:logo)
-    expect(background_image.credit[:logo]).to eq('https://unsplash.com/@andrewcoop')
+    expect(background_image.image[:location]).to eq('denver,co')
+    expect(background_image.image[:image_url]).to eq('https://images.unsplash.com/photo-1599408169542-620fc453382c?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMTMwNjJ8MHwxfHNlYXJjaHwxfHxkZW52ZXJ8ZW58MHx8fHwxNjE1MjUyMTcw&ixlib=rb-1.2.1&q=85')
+
+    expect(background_image.image[:credit]).to be_a Hash
+    expect(background_image.image[:credit]).to have_key(:source)
+    expect(background_image.image[:credit][:source]).to eq('https://unsplash.com/')
+    expect(background_image.image[:credit]).to have_key(:author)
+    expect(background_image.image[:credit][:author]).to eq('Andrew Coop')
+    expect(background_image.image[:credit]).to have_key(:logo)
+    expect(background_image.image[:credit][:logo]).to eq('https://unsplash.com/@andrewcoop')
   end
 end
