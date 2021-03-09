176aca72fcd4f1d09b5c9e6cf7bba6526de30d9b
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 08:35:09 -0700

    Add Trail Route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/forecast', to: 'forecast#show'
       get '/backgrounds', to: 'images#show'
+      get '/trails', to: 'trails#show'
     end
   end
 end
