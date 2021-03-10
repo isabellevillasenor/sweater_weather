90bbb2cd76c63ba50be6845b08c32d710064ebe6
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 17:16:45 -0700

    Add RoadTrip Service Method and Service Spec

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -5,5 +5,11 @@ class MapQuestService
       response = conn.get("/geocoding/v1/address?key=#{ENV['MAPQUEST_API_KEY']}&location=#{location}")
       JSON.parse(response.body, symbolize_names: true)
     end
+
+    def road_trip(params)
+      conn = Faraday.new("https://www.mapquestapi.com")
+      response = conn.get("/directions/v2/route?from=#{params[:origin]}&key=#{ENV['MAPQUEST_API_KEY']}&to=#{params[:destination]}")
+      JSON.parse(response.body, symbolize_names: true)
+    end
   end
 end
