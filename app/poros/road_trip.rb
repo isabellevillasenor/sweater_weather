64118f5a957a814857828d8066132821c378c971
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 19:33:16 -0700

    Add RoadTrip PORO and PORO Spec

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,15 @@
+class RoadTrip
+  attr_reader :start_city,
+              :end_city,
+              :travel_time,
+              :weather_at_eta
+  def initialize(data, start_city, end_city, weather)
+    @start_city = start_city
+    @end_city = end_city
+    @travel_time = data[:route][:formattedTime]
+    @weather_at_eta = {
+      temperature: weather.temp,
+      conditions: weather.conditions
+    }
+  end
+end
