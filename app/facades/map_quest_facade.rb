3a4b36a719e0e0364882c33519e88dc11903e46e
Isabelle Villasenor <villasenori@outlook.com>
2021-03-09 19:15:54 -0700

    Add RoadTrip Method to Facade and Facade Spec

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -4,5 +4,37 @@ class MapQuestFacade
       json = MapQuestService.get_coords(location)
       json[:results][0][:locations][0][:latLng]
     end
+
+    def road_trip(location_params)
+      json = MapQuestService.road_trip(location_params)
+
+      weather = self.destination_weather(location_params[:destination])
+      arrival_time = (Time.now + (json[:route][:realTime]))
+      arrival_weather = self.hourly_destination_weather(arrival_time, weather)
+
+      road_trip = RoadTrip.new(json, location_params[:origin], location_params[:destination], arrival_weather)
+    end
+    
+    def destination_weather(location)
+      destination = MapQuestService.get_coords(location)
+      OpenWeatherService.get_weather(destination[:results][0][:locations][0][:latLng][:lat],
+                                     destination[:results][0][:locations][0][:latLng][:lng])
+    end
+
+    def hourly_destination_weather(data_time, weather)
+      temp = weather[:hourly].select do |hour|
+        time = (Time.at(hour[:dt])).strftime('%H:%M:%S')
+        self.find_time(data_time).strftime('%H:%M:%S') == time
+      end.first
+      HourlyWeather.new(temp)
+    end
+
+    def find_time(data)
+      if data.strftime('%M').to_i < 30
+        data.beginning_of_hour
+      else
+        (data + 60*60).beginning_of_hour
+      end
+    end
   end
 end
--- spec/facades/map_quest_facade_spec.rb
+++ spec/facades/map_quest_facade_spec.rb
@@ -16,5 +16,31 @@ describe MapQuestFacade do
         expect(response[:lng]).to eq(-104.984853)
       end
     end
+
+    it '.road_trip' do
+      VCR.use_cassette('facade_map_2') do
+        params = {
+          origin: 'Denver,Co',
+          destination: 'Pueblo,Co'
+        }
+
+        response = MapQuestFacade.road_trip(params)
+
+        expect(response).to be_a RoadTrip
+        expect(response.start_city).to eq('Denver,Co')
+        expect(response.end_city).to eq('Pueblo,Co')
+        expect(response.travel_time).to eq('01:44:22')
+        expect(response.weather_at_eta[:temperature]).to eq(53.8)
+        expect(response.weather_at_eta[:conditions]).to eq('clear sky')
+      end
+    end
+
+    it '.find_time' do
+      time = MapQuestFacade.find_time(Time.now).strftime('%H:%M:%S')
+      expect(time).to eq('19:00:00')
+
+      else_time = MapQuestFacade.find_time(Time.now + 30*60).strftime('%H:%M:%S')
+      expect(else_time).to eq('20:00:00')
+    end
   end
 end
